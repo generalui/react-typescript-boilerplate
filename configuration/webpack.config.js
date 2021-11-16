6f4101c468ba7ea5371ccaf3f0e25a7b76defefe
Andy Frey <andyfrey95@gmail.com>
2021-11-16 14:31:47 -0700

    Added working source map

--- configuration/webpack.config.js
+++ configuration/webpack.config.js
@@ -1,40 +1,53 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const rootPath = require('./root-path')
+const rootPath = require("./root-path");
 
-module.exports = {
-    entry: path.join(rootPath, "src", "index.tsx"),
-    output: { path: path.join(rootPath, "build"), filename: "index.bundle.js" },
-    mode: process.env.NODE_ENV || "development",
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-    // devServer: { contentBase: path.join(rootPath, "src") },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ["babel-loader"],
-            },
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                use: ["ts-loader"],
-            },
-            {
-                test: /\.(css|scss)$/,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-                use: ["file-loader"],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(rootPath, "public", "index.html"),
-        }),
+const config = {
+  entry: path.join(rootPath, "src", "index.tsx"),
+  output: {
+    path: path.join(rootPath, "build"),
+    filename: "[name].bundle.js",
+    sourceMapFilename: "[name].js.map",
+  },
+  mode: process.env.NODE_ENV || "development",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(rootPath, "public", "index.html"),
+    }),
+  ],
 };
+
+if (process.env.NODE_ENV === "development") {
+  configuration.devtool = "#inline-source-map";
+  configuration.devServer = {
+    overlay: true,
+  };
+}
+
+module.exports = config;
